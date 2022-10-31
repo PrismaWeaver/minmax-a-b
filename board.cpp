0e407ac1948a65108ef809a88f21efdd3f66d24f
Jade Shackelford <j.shackelford@g.austincc.edu>
2022-10-31 17:48:24 -0500

    underlining print

--- board.cpp
+++ board.cpp
@@ -11,6 +11,8 @@
 //        if goal is reached, this returns who won, X or O
 #include "vp.h"
 #include <iostream>
+#define UNDERLINE "\033[4m"
+#define CLOSEUNDERLINE "\033[0m"
 using std::cout;
 using std::endl;
 
@@ -20,33 +22,31 @@ class Board {
         int move;           //holds which board is currently being analyzed
     public:
         Board() {
+            move = 0
+            reset();
+        }
+
+        bool add(string *board[]) { //adds the contents of the array to game if they are valid (meaning only one move has been made between it and the previous board state)
 
         }
 
-        bool add(string *board) { //adds the contents of the array to game if they are valid (meaning only one move has been made between it and the previous board state)
+        int changeDetect (string *board[]) {
 
-        }
-
-        int changeDetect (string *board) {
-            
         }
 
         void reset() { //reset: cleans the board, setting every value to empty, void
             for (int i = 0; i < 9; i++) {
                 for (int u = 0; u < 9; u++) {
-                    game[i][u] = " ";
+                    game[i][u] = "  ";
                 }
             }
         }
 
         void print() { //print: generates a visual of the current board state
             for (int i = 0; i < 9; i++) {
-                cout << " " << game[i][0] << " | " << game[i][1] << " | " << game[i][2] << endl;
-                cout << "----------" << endl;
-                cout << " " << game[i][3] << " | " << game[i][4] << " | " << game[i][5] << endl;
-                cout << "----------" << endl;
+                cout << UNDERLINE << " " << game[i][0] << " | " << game[i][1] << " | " << game[i][2] << " " << CLOSEUNDERLINE << endl;
+                cout << UNDERLINE << " " << game[i][3] << " | " << game[i][4] << " | " << game[i][5] << " " << CLOSEUNDERLINE << endl;
                 cout << " " << game[i][6] << " | " << game[i][7] << " | " << game[i][8] << endl;
-                cout << endl;
             }
         }
 };
