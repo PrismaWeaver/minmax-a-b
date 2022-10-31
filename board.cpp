01914f823e0a1c5c04994ad7c9017413f0f0bc5c
Jade Shackelford <j.shackelford@g.austincc.edu>
2022-10-31 17:37:36 -0500

    more work on board

--- board.cpp
+++ board.cpp
@@ -11,25 +11,24 @@
 //        if goal is reached, this returns who won, X or O
 #include "vp.h"
 #include <iostream>
-#include <algorithm>
+using std::cout;
+using std::endl;
 
 class Board {
     private:
-        string game[9][9]; //an array of board arrays, even indexs are p1's moves odd indexes are p2's
+        string game[9][9];  //an array of board arrays, even indexs are p1's moves odd indexes are p2's
+        int move;           //holds which board is currently being analyzed
     public:
         Board() {
 
         }
 
-        bool add(int index, string move) { //add(index, string): puts the value representing either an X or an O into the array at a given position, returns T/F
-            bool success = false;
-            move = (move);
-            if (index > -1 && index < 9) {
-                if (move == "X" || move == "O") {
+        bool add(string *board) { //adds the contents of the array to game if they are valid (meaning only one move has been made between it and the previous board state)
 
-                }
-            }
-            return success;
+        }
+
+        int changeDetect (string *board) {
+            
         }
 
         void reset() { //reset: cleans the board, setting every value to empty, void
@@ -42,9 +41,12 @@ class Board {
 
         void print() { //print: generates a visual of the current board state
             for (int i = 0; i < 9; i++) {
-                for (int u = 0; u < 9; u++) {
-
-                }
+                cout << " " << game[i][0] << " | " << game[i][1] << " | " << game[i][2] << endl;
+                cout << "----------" << endl;
+                cout << " " << game[i][3] << " | " << game[i][4] << " | " << game[i][5] << endl;
+                cout << "----------" << endl;
+                cout << " " << game[i][6] << " | " << game[i][7] << " | " << game[i][8] << endl;
+                cout << endl;
             }
         }
 };
