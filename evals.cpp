b72f51323fe213126269b71a53905f4718324769
Jade Shackelford <j.shackelford@g.austincc.edu>
2022-11-15 14:53:06 -0600

    updated Twee's eval to be more original

--- evals.cpp
+++ evals.cpp
@@ -60,34 +60,21 @@ int Eval2::getNum(std::string board[], std::string p) { //eval 2 (chris)
 
 };
 
-// class Eval3 : public Eval { //eval 3 (kuda)
-
-// };
+// int Eval3::getNum(string board[], string p) {} //kuda's eval
 
 int Eval4::getNum(string board[], string p) { //Twee's Eval
-    int p1, p2, p3;
-    p1 = p2 = p3 = 0;
-    for (int i = 0; i < 3; i++) {
-        if (board[i * 3] == p || board[(i * 3) + 1] == p || board[(i * 3) + 2] == p) {
-            if (board[i * 3] == p && board[(i * 3) + 1] == p && board[(i * 3) + 2] == p) p3++;
-            else if ((board[i * 3] == p && board[(i * 3) + 1] == p && board[(i * 3) + 2] == " ") || (board[i * 3] == p && board[(i * 3) + 1] == " " && board[(i * 3) + 2] == p) || (board[i * 3] == " " && board[(i * 3) + 1] == p && board[(i * 3) + 2] == p)) p2++;
-            else if ((board[i * 3] == p && board[(i * 3) + 1] == " " && board[(i * 3) + 2] == " ") || (board[i * 3] == " " && board[(i * 3) + 1] == p && board[(i * 3) + 2] == " ") || (board[i * 3] == " " && board[(i * 3) + 1] == " " && board[(i * 3) + 2] == p)) p1++;
-        }
-        if (board[i] == p || board[i + 3] == p || board[i + 6] == p) {
-            if (board[i] == p && board[i + 3] == p && board[i + 6] == p) p3++;
-            else if ((board[i] == p && board[i + 3] == p && board[i + 6] == " ") || (board[i] == p && board[i + 3] == " " && board[i + 6] == p) || (board[i] == " " && board[i + 3] == p && board[i + 6] == p)) p2++;
-            else if ((board[i] == p && board[i + 3] == " " && board[i + 6] == " ") || (board[i] == " " && board[i + 3] == p && board[i + 6] == " ") || (board[i] == " " && board[i + 3] == " " && board[i + 6] == p)) p1++;
-        }
+    int b[9], value = 0;
+    for (int i = 0; i < 9; i++) {
+        if (board[i] == p) b[i] = 4; //square has player's piece
+        else if (board[i] == " ") b[i] = 1; //square is blank
+        else b[i] = 0; //square has opponent's piece
     }
-    if (board[0] == p || board[4] == p || board[8] == p) {
-        if (board[0] == p && board[4] == p && board[8] == p) p3++;
-        else if ((board[0] == p && board[4] == p && board[8] == " ") || (board[0] == p && board[4] == " " && board[8] == p) || (board[0] == " " && board[4] == p && board[8] == p)) p2++;
-        else if ((board[0] == p && board[4] == " " && board[8] == " ") || (board[0] == " " && board[4] == p && board[8] == " ") || (board[0] == p && board[4] == " " && board[8] == " ")) p1++;
+    //multiplies the lines such that lines that have the opponent's piece result in 0 and lines with player piece are valued 4, 16, 64 for amount in line
+    for (int u = 0; u < 3; u++) {
+        value += b[3 * u] * b[(3 * u) + 1] * b[(3 * u) + 2];
+        value += b[u] * b[u + 3] * b[u + 6];
     }
-    if (board[2] == p || board[4] == p || board[6] == p) {
-        if (board[2] == p && board[4] == p && board[6] == p) p3++;
-        else if ((board[2] == p && board[4] == p && board[6] == " ") || (board[2] == p && board[4] == " " && board[6] == p) || (board[2] == " " && board[4] == p && board[6] == p)) p2++;
-        else if ((board[2] == p && board[4] == " " && board[6] == " ") || (board[2] == " " && board[4] == p && board[6] == " ") || (board[2] == p && board[4] == " " && board[6] == " ")) p1++;
-    }
-    return 50 * p3 + 5 * p2 + p1;
+    value += b[0] * b[4] * b[8];
+    value += b[2] * b[4] * b[6];
+    return value;
 }
